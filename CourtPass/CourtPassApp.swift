e1a1463241334f500ece5d72b7c754416c64ead1
Ivan Potyng <nekto_ellez@icloud.com>
2025-09-03 22:24:56 +0300

    Initial Commit

---
+++ CourtPass/CourtPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourtPassApp.swift
+//  CourtPass
+//
+//  Created by Nekto_Ellez on 03.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct CourtPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
